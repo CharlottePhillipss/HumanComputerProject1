ef0c2d0af2bec01a9b7738bfeaf61d14d2d2abf0
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-11-01 15:31:21 -0500

    Update mirror_sketch.js
    Added the weather, date and time display. I still dont know how to get the widgets on there

--- mirror_sketch/mirror_sketch.js
+++ mirror_sketch/mirror_sketch.js
@@ -1,6 +1,14 @@
 let capture;
 let width = 500;
 let height = 500;
+let weather="";
+let json;
+let img;
+
+function preload() {
+  let weather_url="https://api.openweathermap.org/data/2.5/weather?q=Lubbock&units=imperial&APPID=3ef80adea47b29f2cdc09057cb0b1fa6";
+json=loadJSON(weather_url);
+}
 
 function setup() {
   let canvas = createCanvas(500, 500);
@@ -9,6 +17,8 @@ function setup() {
   capture.hide();
   canvas.position(0,0);
   canvas.parent('webcam');
+  weather=json.main.temp;
+  img = loadImage('1.png'); // Load the image
 }
 
 function draw() {
@@ -21,7 +31,16 @@ function draw() {
   
   fill(255);
   textSize(32);
-  text('Hi there!', 0, 50);
-  
+  text(weather+ "\xB0F",10,30);
+  image(img, 20, 50);
+  m = month();
+  d = day();
+  y = year();
+  h = hour();
+  mi = minute();
+  se = second();
+  text(m+'/'+d+'/'+y,357,30);
+  textSize(25);
+  text(h+':'+mi+':'+se,400,60);
   
 }
